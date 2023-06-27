f2dae056d71058b4076683049fdc577108b09abe
AL19man <136197729+AL19man@users.noreply.github.com>
2023-06-27 22:22:55 +0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<form method="post" action="login.php">
+User: <input type="text" name="userid"><br>
+Password: <input type="password" name="password"><br>
+<input type="submit" value="Validate">
+</form>
+
+</body>
+</html>
